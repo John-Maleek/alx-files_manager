ce09b786bcc99750b55f5f8269956c337001a989
John Maleek <jmzoran01@gmail.com>
2024-07-03 00:27:57 +0100

    task 0

---
+++ utils/redis.js
@@ -0,0 +1,48 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.log(err);
+      this.isConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async get(key) {
+    let data;
+    try {
+      data = this.client.get(key);
+    } catch (error) {
+      console.log(error);
+    }
+    return data;
+  }
+
+  async set(key, value, duration) {
+    try {
+      this.client.setex(key, duration, value);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async del(key) {
+    try {
+      await this.client.del(key);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
